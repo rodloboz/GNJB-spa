eaec361eec45b79b84c114aafb784fb8f693ed9d
Rui Freitas <dinnerpartysaboteur@gmail.com>
2017-12-03 21:15:27 +0000

    media screen 1280px

--- js/application.js
+++ js/application.js
@@ -34,67 +34,14 @@ $(document).ready(function() {
 
   // Cards
 
-    $('#card-1').on('click touchstart', function() {
-      console.log("hey");
+    $('#drag-left svg').on('click touchstart', switchCard);
+
+    $('#drag-right svg').on('click touchstart', switchCard);
+
+    function switchCard() {
       $('#card-1').toggleClass('front').toggleClass('back');
-      $('#card-2').toggleClass('front').toggleClass('back');
-    });
-
-    $('#card-2').on('click touchstart', function() {
-      console.log("hey");
-      $('#card-1').toggleClass('front').toggleClass('back');
-      $('#card-2').toggleClass('front').toggleClass('back');
-    });
-
-  var startPos = {x: 0, y: 0, range: Infinity };
-
-  interact('.draggable')
-  .draggable({
-    // enable inertial throwing
-    inertia: true,
-    // enable autoScroll
-    restrict: {
-      restriction: "parent",
-      endOnly: true,
-      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-    },
-    autoScroll: true,
-    // call this function on every dragmove event
-    onmove: dragMoveListener,
-    // call this function on every dragend event
-    onstart: getStartPos,
-  });
-
-  function dragMoveListener (event) {
-    var target = event.target,
-        // keep the dragged position in the data-x/data-y attributes
-        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-
-    // translate the element
-    target.style.webkitTransform =
-    target.style.transform =
-      'translate(' + x + 'px, ' + y + 'px)';
-
-    // update the posiion attributes
-    target.setAttribute('data-x', x);
-    target.setAttribute('data-y', y);
-  }
-
-  function getStartPos (event) {
-    var rect = interact.getElementRect(event.target);
-
-    // record center point when starting a drag
-    startPos.x = rect.left + rect.width  / 2;
-    startPos.y = rect.top  + rect.height / 2;
-
-    console.log(startPos);
-  }
-
-  interact('.drag-info').dropzone({
-    // only accept elements matching this CSS selector
-    accept: '.draggable',
-    // Require a 75% element overlap for a drop to be possible
-    overlap: 0.75
-  });
+      $('#card-2').toggleClass('back').toggleClass('front');
+      $('#pagination li a:contains("1")').toggleClass('active');
+      $('#pagination li a:contains("2")').toggleClass('active');
+    };
 });
