4e84bd0202a8a31912445f385c5f17699eabb536
Rui Freitas <dinnerpartysaboteur@gmail.com>
2017-12-04 09:50:09 +0000

    added mute unmute controls

--- js/application.js
+++ js/application.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
 
+
+  // Nav
+
   var toggleBurger = function() {
     $('.burger').toggleClass('open');
     $('#nav').toggleClass('open');
@@ -20,17 +23,51 @@ $(document).ready(function() {
     }, 2000);
   })
 
+  // Video
+
+  var videoDOM = $('.video-wrapper');
   var screenWidth = $(window).width();
+  var muted = false;
   if (screenWidth < 800){
-    $('.video').removeAttr('autoplay');
+    videoDOM.removeAttr('autoplay');
   } else {
-    $('.video').attr('autoplay');
+    videoDOM.attr('autoplay');
   }
   var vid = $('video')[0];
   $(window).scroll(function(){
     var scroll = $(this).scrollTop();
     scroll > 300 ? vid.pause() : vid.play()
   });
+
+  videoDOM.mouseenter(function() {
+    $('.video-icons').addClass("show");
+  });
+
+  videoDOM.mouseleave(function() {
+    $('.video-icons').removeClass("show");
+  });
+
+  $('.video-icons').click(function() {
+    if (!muted) {
+      $('video').prop("muted", true);
+      $(this).removeClass('unmuted').addClass('muted');
+      muted = true;
+    } else if (muted) {
+      $('video').prop("muted", false);
+      $(this).removeClass('muted').addClass('unmuted');
+      muted = false;
+    }
+  });
+
+  // function blink(){
+  //     $('.skip-icon').delay(100).fadeTo(100,0.5).delay(100).fadeTo(100,1, blink);
+  // }
+
+  // blink();
+
+  // $('.skip-icon').click()
+
+
 
   // Cards
 
